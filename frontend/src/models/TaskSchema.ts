5c3f38f815c205c91d8dd238015d6c4bf4bb0366
SlipperyLion <lion@slepichev>
2025-04-23 20:06:59 +0300

    Setup List/Task objects for later use

---
+++ frontend/src/models/TaskSchema.ts
@@ -0,0 +1,10 @@
+export interface TaskSchema {
+    id: number;
+    title: string;
+    is_checked: boolean;
+    is_priority: boolean;
+    list_id: number;
+    created_at: string;
+    updated_at: string;
+
+}
