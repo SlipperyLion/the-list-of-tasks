5922a176f120a9476e142de77b0351dc018a8c3f
SlipperyLion <lion@slepichev>
2025-04-25 18:52:55 +0300

    Create ListForm, add Styles for Modal and ListForm

---
+++ frontend/src/components/List/ListForm.tsx
@@ -0,0 +1,49 @@
+import React, {useState} from "react";
+import "../../styles/ListFormStyle.css"
+interface ListFormProps{
+    onSubmit: (title: string, description: string) => void;
+    closeModal: () => void;
+}
+
+
+function ListForm({onSubmit, closeModal}: ListFormProps){
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("");
+
+    function handleSubmit(event: React.FormEvent){
+        event.preventDefault();
+        onSubmit(title, description);
+        closeModal();
+    }
+
+    return(
+        <form onSubmit={handleSubmit} className="notebook-form">
+            <div className="holes">
+                <div className="hole"></div>
+                <div className="hole"></div>
+                <div className="hole"></div>
+                <div className="hole"></div>
+                <div className="hole"></div>
+                <div className="hole"></div>
+            </div>
+            <div className="form-content">
+                <label>Create new list</label>
+                <label>
+                    Title:
+                    <input value={title} maxLength={128} onChange={(e) => setTitle(e.target.value)} required/>
+                </label>
+                <label>
+                    Description:
+                    <input value={description} maxLength={512} onChange={(e) => setDescription(e.target.value)}/>
+                </label>
+                <div className="form-buttons">
+                    <button type="submit" className="save-button">Create</button>
+                    <button type="button" onClick={() => closeModal()} className="cancel-button">Cancel</button>
+                </div>
+            </div>
+        </form>
+
+    )
+}
+
+export default ListForm;
