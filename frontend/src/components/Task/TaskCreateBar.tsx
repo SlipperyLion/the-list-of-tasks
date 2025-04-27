80d59a26d6063d6817d93242811f96b7e1eb353f
SlipperyLion <lion@slepichev>
2025-04-28 00:39:15 +0300

    Add hover for logo wrapper. Add better feedback for Create button in ListDetailPage

--- frontend/src/components/Task/TaskCreateBar.tsx
+++ frontend/src/components/Task/TaskCreateBar.tsx
@@ -7,15 +7,20 @@ interface TaskCreateBarProps {
 
 function TaskCreateBar({onSubmit}: TaskCreateBarProps) {
     const [title, setTitle] = useState("");
+    const [isClicked, setIsClicked] = useState(false);
     function handleSubmit(event: React.FormEvent){
         event.preventDefault();
         onSubmit(title);
         setTitle("");
+        setIsClicked(true);
+        setTimeout(() => {
+            setIsClicked(false);
+        }, 150);
     }
     return (
         <form className="task-create-container" onSubmit={handleSubmit}>
             <input type="text" value={title} maxLength={128} className="task-input" onChange={(e) => setTitle(e.target.value)} required placeholder="New task title..."/>
-            <button type="submit" className="create-button"  >+ Create</button>
+            <button type="submit" className={`create-button ${isClicked ? 'shrunk' : ''}`} >+ Create</button>
         </form>
     )
 }
