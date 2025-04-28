9391c074473b14c0f874a1f4f205fbeb40d840a1
SlipperyLion <lion@slepichev>
2025-04-28 15:05:41 +0300

    Add footer and license

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,7 @@ import Lists from "./pages/Lists.tsx";
 import About from './pages/About'
 import ListDetailPage from "./pages/ListDetailPage.tsx";
 import Header from "./components/UI/Header.tsx"
-
+import Footer from "./components/UI/Footer.tsx"
 
 function App() {
 
@@ -12,11 +12,14 @@ function App() {
     <>
         <Router>
             <Header/>
-            <Routes>
-                <Route path="/" element={<Lists />} />
-                <Route path="/about" element={<About />} />
-                <Route path="/list/:list_id_string" element={<ListDetailPage />} />
-            </Routes>
+            <div className="app-content">
+                <Routes>
+                    <Route path="/" element={<Lists />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/list/:list_id_string" element={<ListDetailPage />} />
+                </Routes>
+            </div>
+            <Footer/>
         </Router>
     </>
   );
